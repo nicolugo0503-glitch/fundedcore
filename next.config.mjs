a7db159da3952987923e8d3a766b34dc4d291215
nicolugo0503-glitch <nicolugo0503@gmail.com>
2026-05-31 01:18:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/fundedcore",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  reactStrictMode: true,
+};
+export default nextConfig;
+/** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true };
 export default nextConfig;
